b45cf151488fcac89d89d6e86df3fd7e3e2dec29
Pawel Zukowicz <junzuk2000@gmail.com>
2021-05-25 07:51:51 +0200

    first commit

---
+++ WinFormsAppLaboratoriaProg3_zad1/WinFormsAppLaboratoriaProg3_zad1/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinFormsAppLaboratoriaProg3_zad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float suma = float.Parse(Textbox1.Text) + float.Parse(TextBox2.Text);
+                TextBoxWynik.Text = "Suma " + Textbox1.Text.ToString() + " + " + TextBox2.Text.ToString() + " = " + suma.ToString(); 
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("nie prowadzono poprawych liczb !");
+            }
+        }
+
+        private void btnRoznica_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float suma = float.Parse(Textbox1.Text) - float.Parse(TextBox2.Text);
+                TextBoxWynik.Text = "Różnica " + Textbox1.Text.ToString() + " - " + TextBox2.Text.ToString() + " = " + suma.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nie prowadzono poprawych liczb !");
+            }
+        }
+
+        private void btnIloczyn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float suma = float.Parse(Textbox1.Text) * float.Parse(TextBox2.Text);
+                TextBoxWynik.Text = "Iloczyn " + Textbox1.Text.ToString() + " * " + TextBox2.Text.ToString() + " = " + suma.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nie prowadzono poprawych liczb !");
+            }
+        }
+
+        private void btnIloraz_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float suma = float.Parse(Textbox1.Text) / float.Parse(TextBox2.Text);
+                TextBoxWynik.Text = "Iloraz" + Textbox1.Text.ToString() + " / " + TextBox2.Text.ToString() + " = " + suma.ToString();
+            }
+            catch (DivideByZeroException)   // trochę śmiesznie bo ignoruje on zupełnie fakt że podzielił przez zero i odpowiada jak gdyby nigdy nic wynikiem nieskończoności 
+            {
+                MessageBox.Show("Przez zero nie dzielimy !");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nie prowadzono poprawych liczb !");
+            }
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TekstBoxWynik_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBoxWynik_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ WinFormsAppLaboratoriaProg3_zad2/WinFormsAppLaboratoriaProg3_zad2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinFormsAppLaboratoriaProg3_zad2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnOgnia_Click(object sender, RoutedEventArgs e)
+        {
+            if(ListBox1.SelectedIndex < 0 && ListBox2.SelectedIndex < 0)
+            {
+                MessageBox.Show("Kapitanie, ale jaki CeL i która Torpeda?!");
+            }
+            else
+            {
+                if (btnUzbrojona.IsChecked == true && btnUzbrojona.IsChecked == true)
+                {
+                    MessageBox.Show("Wystrzelono " + ListBox2.SelectedItem.ToString() + " w stronę okrętu " + ListBox1.SelectedItem.ToString());
+                    ListBox2.Items.RemoveAt(ListBox2.SelectedIndex);
+                    Task.Delay(1000);
+                    if (ListBox2.SelectedIndex == 1)
+                    {
+                        MessageBox.Show("Kapitanie, list z dowództwa. Niniejszym odsowam Pana ze stanowiska Kapitana. Niech się Pan modli by dotrzeć na ląd cało ! *!^@!");
+                        ListBox1.Items.RemoveAt(ListBox1.SelectedIndex);
+                    }
+                    else
+                    {
+                        TextBox1.Text = ("Zniszczyliśmy okręt " + ListBox1.SelectedItem.ToString());
+                        ListBox1.Items.RemoveAt(ListBox1.SelectedIndex);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kapitanie, ale Torpeda nie jest gotowa do strzału!");
+                }
+            }
+        }
+    }
+}
